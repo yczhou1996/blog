42ae1b79841195c36928a8e276cfd45a6ccf0e5a
zyc <648922974@qq.com>
2018-09-03 13:42:29 +0800

    index.

--- src/main/java/com/ycz/controller/IndexController.java
+++ src/main/java/com/ycz/controller/IndexController.java
@@ -1,8 +1,12 @@
 package com.ycz.controller;
 
+import com.ycz.model.Vo.UserVo;
+import com.ycz.utils.CommonsUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Chitanda Eru on 2018/8/16.
@@ -12,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @GetMapping(value = "/index")
-    public String index(){
+    public String index(HttpServletRequest request){
+        UserVo userVo = CommonsUtil.getLoginUser(request);
+        request.setAttribute("user", userVo);
         return "index";
     }
 
